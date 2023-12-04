e9e138c467965cd4b249ce6d290c9ee637b5ecdf
griseldapc <boosycen@gmail.com>
2023-12-04 14:52:19 +0700

    update

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,53 +1,54 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import '../dist/css/footer.css';
 
 export class Footer extends Component {
     render() {
         return (
             <main>
-                <Container style={{paddingTop: "30px"}}>
-                    <Row className="justify-content-between">
-                        <Col xs={12} lg={2} className="-mx-6">
-                            <div className="px-6">
+                <Container className='footer'>
+                    <Row className="cover-footer justify-content-between">
+                        <Col xs={12} lg={2} className="judul-footer -mx-6">
+                            <div className="bungkus-footer px-6">
                                 <img src="../../asset/header-footer/footer.png" alt="" />
-                                <p className="mt-4" style={{width: "400px", color: '#1C2661'}}>
+                                <p className="text-footer mt-4">
                                     Jelajahi semua pengetahuan dan ilmu yang berguna bersama kami, untuk kehidupan yang lebih bermanfaat
                                 </p>
                             </div>
                         </Col>
 
-                        <Col lg={6} className="">
-                            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-                                <div className="col">
-                                    <h1 className="mb-4 fs-5 fw-semibold" style={{color: '#1C2661'}}>Informasi</h1>
-                                    <p className='d-flex flex-column'>
-                                        <a href="#" className="text-decoration-none mb-4" style={{color: "#404040"}}>Artikel</a>
-                                        <a href="#" className="text-decoration-none"style={{color: "#404040"}}>Galeri</a>
+                        <Col lg={6} className="informasi">
+                            <div className="row row-cols-1 row-cols-md-3 row-cols-lg-3">
+                                <div className="col-informasi">
+                                    <h1 className="bungkus-informasi mb-4 fs-5 fw-semibold">Informasi</h1>
+                                    <p className='menu-informasi d-flex flex-column'>
+                                        <a href="#" className="menu text-decoration-none mb-4">Artikel</a>
+                                        <a href="#" className="menu text-decoration-none">Galeri</a>
                                     </p>
 
                                 </div>
 
-                                <div className="col">
-                                    <h1 className="mb-4 fs-5 fw-semibold" style={{color: '#1C2661'}}>Tentang</h1>
-                                    <p className='d-flex flex-column'>
-                                        <a href="#" className="text-decoration-none mb-4"style={{color: "#404040"}}>Tentang kami</a>
-                                        <a href="#" className="text-decoration-none"style={{color: "#404040"}}>Hubungi kami</a>
+                                <div className="col-tentang">
+                                    <h1 className="bungkus-tentang mb-4 fs-5 fw-semibold">Tentang</h1>
+                                    <p className='menu-tentang d-flex flex-column'>
+                                        <a href="#" className="menu text-decoration-none mb-4">Tentang kami</a>
+                                        <a href="#" className="menu text-decoration-none">Hubungi kami</a>
                                     </p>
                                 </div>
 
-                                <div className="col">
-                                    <h1 className="mb-4 fs-5 fw-semibold" style={{color: '#1C2661'}}>Ikuti Kami</h1>
-                                    <a href="#" className="d-flex text-decoration-none mb-4"style={{color: "#404040"}}>
-                                        <img src="../../asset/header-footer/fb.png" className=" me-3" alt="Facebook" style={{ height: "17px", marginTop: "5px"}} />
+                                <div className="col-ikuti">
+                                    <h1 className="bungkus-ikuti mb-4 fs-5 fw-semibold">Ikuti Kami</h1>
+                                    <a href="#" className="menu-ikuti d-flex text-decoration-none mb-4">
+                                        <img src="../../asset/header-footer/fb.png" className="logo-sekolah me-3" alt="Facebook"/>
                                         namasekolah
                                     </a>
-                                    <a href="#" className="d-flex text-decoration-none mb-4"style={{color: "#404040"}}>
-                                        <img src="../../asset/header-footer/tw.png" className=" me-3" alt="Twitter" style={{ height: "17px", marginTop: "5px"}}/>
+                                    <a href="#" className="menu-ikuti d-flex text-decoration-none mb-4">
+                                        <img src="../../asset/header-footer/tw.png" className="logo-sekolah me-3" alt="Twitter"/>
                                         namasekolah
                                     </a>
-                                    <a href="#" className="d-flex text-decoration-none"style={{color: "#404040"}}>
-                                        <img src="../../asset/header-footer/ig.png" className=" me-3" alt="Instagram" style={{ height: "17px", marginTop: "5px"}}/>
+                                    <a href="#" className="menu-ikuti d-flex text-decoration-none">
+                                        <img src="../../asset/header-footer/ig.png" className="logo-sekolah me-3" alt="Instagram"/>
                                         namasekolah
                                     </a>
                                 </div>
@@ -55,10 +56,10 @@ export class Footer extends Component {
                         </Col>
                     </Row>
 
-                    <hr className="my-6 border-none" style={{marginTop: "30px"}} />
+                    <hr className="garis my-6 border-none"/>
 
                     <div>
-                        <p className="text-center fs-6">Copyright © 2021. All rights reserved.</p>
+                        <p className="copy text-center ">Copyright © 2021. All rights reserved.</p>
                     </div>
                 </Container>
             </main>
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,5 +1,6 @@
 import { Container, Nav, Navbar, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import '../dist/css/navbar.css';
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -31,26 +32,26 @@ export default function Header() {
           background: scrolling
             ? 'linear-gradient(78.96deg, #4BA3EB 2.22%, #5DDAAA 98.48%)'
             : 'transparent',
-          transition: 'background 0.3s',
+          transition: 'background 0.3s'
         }}
         variant="dark"
       >
-        <Container style={{ marginTop: "20px", marginBottom: "20px" }}>
-          <Navbar.Brand href="#home" style={{ zIndex: 1000, marginLeft: "-8px" }}>
+        <Container className="cover-navbar">
+          <Navbar.Brand className="navbar-logo" href="#home">
             <img src="../../asset/header-footer/header.png" alt="" />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+          <Navbar.Toggle className="responsive-navbar" aria-controls="responsive-navbar-nav"/>
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mx-auto gap-3 align-items-center" style={{ zIndex: 1000, fontSize: "17px" }}>
+            <Nav className="navbar-menu mx-auto gap-3 align-items-center">
               <Nav.Link href="/" style={{ color: 'white' }}>Beranda</Nav.Link>
               <Nav.Link href="/tentang" style={{ color: 'white' }}>Tentang</Nav.Link>
               <Nav.Link href="/galeri" style={{ color: 'white' }}>Galeri</Nav.Link>
               <Nav.Link href="/artikel" style={{ color: 'white' }}>Artikel</Nav.Link>
               <Nav.Link href="/kontak" style={{ color: 'white' }}>Kontak</Nav.Link>
             </Nav>
-            <Nav style={{ zIndex: 1000, marginRight: "-8px" }}>
+            <Nav className="button-navbar-cover">
               <Nav.Link href="#">
-                <Button className="py-1 px-4" style={{ background: '#FFFFFF66', border: 'none', marginRight: "-10px" }}>PPDB</Button>
+                <Button className="button-navbar py-1 px-4">PPDB</Button>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
